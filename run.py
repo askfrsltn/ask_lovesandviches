0cc9242656e00082e113614c57ef9b1894368ce1
askfrsltn <saltanchuk@gmail.com>
2021-09-20 18:35:08 +0000

    get last 5 entries from sales sheet

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-# from pprint import pprint
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -82,6 +82,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    collect last 5 entries from the sales sheet
+    return list of lists
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+    
 
 def main():
     """
@@ -95,4 +109,5 @@ def main():
     
 
 print("Welcome to love sandviches data automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
